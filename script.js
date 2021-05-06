d36b3f8b3d2da09988a95abcc0cffefa58d2739f
Thalles Carneiro <thallescarneiro@gmail.com>
2021-05-06 15:09:20 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ document.addEventListener('click', (event) => {
   if (event.target.id === 'criar-tarefa') {
     addTasks();
   };
-
+  if (event.target.classList.contains('task')) {
+    const getEventElement = event.target;
+    selectTask(getEventElement);
+  };
 
 });
 
@@ -12,7 +15,18 @@ function addTasks() {
   const getInputValue = getInputElement.value;
   const newTask = document.createElement('li');
   newTask.innerHTML = getInputValue;
+  newTask.className = 'task';
   getTasksList.appendChild(newTask);
   const clearInput = '';
   getInputElement.value = clearInput;
+}
+
+function selectTask(selectedTask) {
+  const selectedTaskElement = document.querySelector('.selected');
+  if (selectedTaskElement === null) {
+    selectedTask.classList.add('selected');
+  } else {
+    selectedTaskElement.classList.remove('selected');
+    selectedTask.classList.add('selected');
+  };
 }
