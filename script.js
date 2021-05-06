2ff32b59cdf304f9b7faf81c00eaf748a6b2cf76
Thalles Carneiro <thallescarneiro@gmail.com>
2021-05-06 15:22:34 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ document.addEventListener('click', (event) => {
 
 });
 
+document.addEventListener('dblclick', (event) => {
+  if (event.target.classList.contains('task')) {
+    const getEventElement = event.target;
+    completeTask(getEventElement)
+  };
+});
+
+
 function addTasks() {
   const getTasksList = document.getElementById('lista-tarefas');
   const getInputElement = document.getElementById('texto-tarefa');
@@ -29,4 +37,17 @@ function selectTask(selectedTask) {
     selectedTaskElement.classList.remove('selected');
     selectedTask.classList.add('selected');
   };
+}
+
+function completeTask(completedTask) {
+  const completedTaskElement = document.querySelector('.completed');
+  if (completedTask.classList.contains('completed')) {
+    completedTask.classList.remove('completed');
+  } else {
+    if (completedTaskElement === null) {
+      completedTask.classList.add('completed');
+    } else {
+      completedTask.classList.add('completed');
+    };
+  }
 }
