ff5cd54e794b213b1634e5eb112b9e5a8d4216f3
Thalles Carneiro <thallescarneiro@gmail.com>
2021-05-06 23:29:34 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
+recoverTasksValues();
+
 document.addEventListener('click', (event) => {
   if (event.target.id === 'criar-tarefa') {
     addTasks();
-  };
+  }
   if (event.target.classList.contains('task')) {
     const getEventElement = event.target;
     selectTask(getEventElement);
-  };
+  }
   if (event.target.id === 'apaga-tudo') {
     const getToDoList = document.getElementById('lista-tarefas');
     getToDoList.innerHTML = '';
-  };
+  }
   if (event.target.id === 'remover-finalizados') {
     const completedList = document.querySelectorAll('.completed');
     console.log('Verificando lista completa de completed:', completedList);
     clearCompletedTasks(completedList);
-  };
+  }
+  if (event.target.id === 'salvar-tarefas') {
+    saveToDoList();
+  }
 });
 
 document.addEventListener('dblclick', (event) => {
   if (event.target.classList.contains('task')) {
     const getEventElement = event.target;
     completeTask(getEventElement)
-  };
+  }
 });
 
 function addTasks() {
@@ -43,7 +48,7 @@ function selectTask(selectedTask) {
   } else {
     selectedTaskElement.classList.remove('selected');
     selectedTask.classList.add('selected');
-  };
+  }
 }
 
 function completeTask(completedTask) {
@@ -55,12 +60,28 @@ function completeTask(completedTask) {
       completedTask.classList.add('completed');
     } else {
       completedTask.classList.add('completed');
-    };
+    }
   }
 }
 
 function clearCompletedTasks(completedItems) {
   for (let index = 0; index < completedItems.length; index += 1) {
     completedItems[index].remove();
-  };
+  }
+}
+
+/* Consultei o Pull Request do Luciano Almeida Andrade Pereira para resolver essa parte.
+https://github.com/tryber/sd-012-project-todo-list/pull/71
+*/
+function saveToDoList() {
+  const getTasksList = document.getElementById('lista-tarefas');
+  localStorage.setItem('TaskList', getTasksList.innerHTML);
+}
+
+function recoverTasksValues() {
+  const getTasksList = document.getElementById('lista-tarefas');
+  const tasksSaved = localStorage.getItem('TaskList');
+  if (tasksSaved !== null) {
+    getTasksList.innerHTML = tasksSaved;
+  }
 }
